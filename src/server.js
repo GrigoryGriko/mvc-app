749423bfdae3d61ddc764feca212868e54410717
GrigoryGriko <griko1996@gmail.com>
2024-03-05 22:50:02 +0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path');
+const userController = require('./domain/users/controller')
+
 
 const PORT = 5000;
 
@@ -8,6 +10,9 @@ const app = express();
 
 app.set("view engine", "hbs");
 app.set("views", path.resolve(__dirname, 'views'));
+
+
+app.get('/users', userController.getAll)
 
 
 app.listen(PORT, () => console.log('сервер запущен на порту ' + PORT))
